242f5be7b060239d222218d7dd91573096a6cd42
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-07 11:21:49 -0500

    Create add setup method and add_interest_test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -4,6 +4,11 @@ require './lib/exhibit'
 require './lib/patron'
 
 class PatronTest < Minitest::Test
+  def setup
+    @patron = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+  end
 
   def test_it_exists_with_readable_attributes
     patron = Patron.new("Bob", 20)
@@ -12,5 +17,10 @@ class PatronTest < Minitest::Test
     assert_equal "Bob", patron.name
     assert_equal 20, patron.spending_money
     assert_empty patron.interests
+  end
+
+  def test_it_can_add_interests
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
   end
 end

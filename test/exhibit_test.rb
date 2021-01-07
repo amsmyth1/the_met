0a7c43c1ba9d0ea20d6c0359b9ec73929b22ec50
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-07 11:14:42 -0500

    Initialize exhibit class with readers

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -9,13 +9,14 @@ class ExhibitTest < Minitest::Test
   end
 
   def test_it_exists
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-    assert_instance_of Exhibit, @exhibit
+    assert_instance_of Exhibit, exhibit
   end
 
   def test_it_has_readable_attributes
 
     assert_equal "Gems and Minerals", @exhibit.name
     assert_equal 0, @exhibit.cost
-  end 
+  end
 end

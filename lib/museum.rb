a60853b26d6a65b621d169cf3c5238f63dc8b5e6
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-07 11:54:08 -0500

    Refactor enumerables to select

--- lib/museum.rb
+++ lib/museum.rb
@@ -13,21 +13,9 @@ class Museum
 
   def recommend_exhibits(patron)
     @exhibits.select do |exhibit|
-      # require 'pry'; binding.pry
       exhibit.name == exhibit_match(exhibit, patron)[0]
     end
   end
-
-  # def exhibit_match(exhibit, patron)
-  #   @match = []
-  #   patron.interests.each do |interest|
-  #     require 'pry'; binding.pry
-  #     if interest == exhibit.name
-  #       @match << exhibit
-  #     end
-  #   end
-  #   @match
-  # end
 
   def exhibit_match(exhibit, patron)
     patron.interests.select do |interest|

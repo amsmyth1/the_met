b91e2d1354aa04cfe1fc979df7303d3b5cddab3e
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-07 11:53:44 -0500

    Complete recommend_exhibits method and pass tests

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,29 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    @exhibits.select do |exhibit|
+      # require 'pry'; binding.pry
+      exhibit.name == exhibit_match(exhibit, patron)[0]
+    end
+  end
+
+  # def exhibit_match(exhibit, patron)
+  #   @match = []
+  #   patron.interests.each do |interest|
+  #     require 'pry'; binding.pry
+  #     if interest == exhibit.name
+  #       @match << exhibit
+  #     end
+  #   end
+  #   @match
+  # end
+
+  def exhibit_match(exhibit, patron)
+    patron.interests.select do |interest|
+      interest == exhibit.name
+    end
+  end
+
 end

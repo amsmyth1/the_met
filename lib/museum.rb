20fb714af486d1de85beb5daef946b1835d1d3ff
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-07 11:59:52 -0500

    Create admit method and pass tests

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -23,4 +25,7 @@ class Museum
     end
   end
 
+  def admit(patron)
+    @patrons << patron 
+  end
 end

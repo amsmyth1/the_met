e00a846fbf9866624dacd564e5538bd103de3c14
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-07 11:18:33 -0500

    Create readable attribute test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -5,9 +5,11 @@ require './lib/patron'
 
 class PatronTest < Minitest::Test
 
-  def test_it_exists
+  def test_it_exists_with_readable_attributes
     patron = Patron.new("Bob", 20)
 
     assert_instance_of Patron, patron
+    assert_equal "Bob", patron.name
+    assert_equal 20, patron.spending_money 
   end
-end 
+end
